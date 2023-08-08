72591bfd96fa0b68c8fda714e3125733a28da80e
kaung <kaungmyathun7@gmail.com>
2023-08-08 10:53:18 +0630

    adding: new photos

--- components/HeroContent/HeroContent.tsx
+++ components/HeroContent/HeroContent.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import styles from "./HeroContent.module.css";
+import Image from "next/image";
+import primogemLogo from "/public/image/primogem.webp";
 
 type Props = {};
 
 export default function HeroContent({}: Props) {
-  return <div className={`${styles.contentContainer}`}>HeroContent</div>;
+  return (
+    <div className={`${styles.contentContainer}`}>
+      <Image
+        src={primogemLogo}
+        alt="primogem"
+        className={styles.primogem}
+      ></Image>
+      <div className={styles.dropDown}></div>
+    </div>
+  );
 }
