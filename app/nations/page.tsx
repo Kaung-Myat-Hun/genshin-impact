9ef0ed09d63b4533ef8ce12fef260d0dae09e2c9
kaung <kaungmyathun7@gmail.com>
2023-08-11 17:00:57 +0630

    adding: new page design

--- app/nations/page.tsx
+++ app/nations/page.tsx
@@ -1,7 +1,169 @@
 import React from "react";
+import styles from './style.module.css'
+import FilterComponent from '../../components/Filters/FilterComponent'
+import {Card} from '@/components/Re_usable/Card/Card'
 
 type Props = {};
 
-export default function page(props: Props) {
-  return <div>page</div>;
+export default function page({}: Props) {
+  const getData = (data: Object[]) =>{
+    console.log(data);
+  }
+  const arr = [
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },{
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+    {
+      avatar : "this is avatar",
+      cha_name: "this is chaname",
+      vision: "this is vision"
+    },
+
+  ]
+  return (
+    <div className={styles.characterContainer}>
+      
+    </div>
+  );
 }
