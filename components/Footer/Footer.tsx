4aa52e82063fb5cbff689c182f6ff4a3297381ba
kaung <kaungmyathun7@gmail.com>
2023-08-11 11:27:34 +0630

    adding: new design and explore btn

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import Image from "next/image";
 import styles from "./Footer.module.css";
 import hoyoverseLogo from "@/public/image/hoyoverse-logo.png";
-import Link from 'next/link'
+import Link from 'next/link';
+import microsoftIcon from '../../public/image/icons/micorsoft.png';
+import appleIcon from '../../public/image/icons/apple.png';
+import androidIcon from '../../public/image/icons/android.png';
+import playstationIcon from '../../public/image/icons/playstation.png';
 
 type Props = {};
 
 export default function Footer({}: Props) {
   return (
-    <div className={`bg-slate-900 ${styles.footercontainer}`}>
-      <div className={`w-9/12 mx-auto py-6 flex justify-between`}>
-        <div>
+    <div className={` bg-slate-950 ${styles.footercontainer}`}>
+      <div className={`w-9/12 mx-auto py-4 flex justify-between`}>
+        <div className={`pt-5`}>
           <Image
             src={hoyoverseLogo}
             alt="hoyoverse"
@@ -20,11 +24,25 @@ export default function Footer({}: Props) {
           <p className={`text-sm text-center`}>Credits to <br /> <Link href="https://hoyoverse.com/">hoyoverse.com</Link></p>
         </div>
         <div className={`flex flex-col text-center items-center`}>
-          <p>Cross Platform RPG</p>
-          <p>Avaliable at : </p>
+          <p>Cross Platform RPG Game</p>
+          <p className={`flex items-center justify-center`}> 
+          <p>Available at : </p>
+            <Link href="">
+              <Image src={microsoftIcon} alt="microsoft" className={styles.microsoftIcon}></Image>
+            </Link>
+            <Link href="">
+              <Image src={appleIcon} alt="microsoft" className={styles.appleIcon}></Image>
+            </Link>
+            <Link href="">
+              <Image src={androidIcon} alt="microsoft" className={styles.androidIcon}></Image>
+            </Link>
+            <Link href="">
+              <Image src={playstationIcon} alt="microsoft" className={styles.playstationIcon}></Image>
+            </Link>
+          </p>
           <small>Redesign and created by <Link href="https://github.com">Bate Thar</Link></small>
         </div>
-        <div className={`text-center`}>
+        <div className={`text-center pt-5`}>
           <Link href="/"><small>Home</small></Link> <br />
           <Link href="/contact"><small>Contact</small></Link> <br />
           <Link href="https://genshin.hoyoverse.com"><small>Go to official site</small></Link>
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from "./Hero.module.css";
+import Link from 'next/link';
 import Image from "next/image";
 import bgVideo from "../../public/video/main-bg.gif";
-
+import btnExplore from '../../public/image/btn.webp'
 type Props = {};
 
 export default function Hero({}: Props) {
@@ -17,6 +18,12 @@ export default function Hero({}: Props) {
             alt="bg image"
             className={styles.video}
           ></Image>
+          <div className={styles.btnOuterContainer}>
+            <div className={styles.btnContainer}>
+              <Image src={btnExplore} alt="btn explore" className={styles.btnExplore}/>
+              <p className={styles.btnText}><Link href="/characters">Explore</Link></p>
+            </div>
+          </div>
           <div className={styles.motionDivBg}></div>
         </div>
       </div>
