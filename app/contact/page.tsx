1caf794142bd1ba9ac4667f7e9a11a75a76557df
kaung <kaungmyathun7@gmail.com>
2023-09-25 14:40:44 +0630

    adding: new image at nation page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,6 +5,6 @@ type Props = {}
 
 export default function page  ({}: Props)  {
   return (
-    <div>page</div>
+    <div className={styles.characterContainer}>page</div>
   )
 }
--- app/nations/page.tsx
+++ app/nations/page.tsx
@@ -1,169 +1,55 @@
 import React from "react";
 import styles from './style.module.css'
-import FilterComponent from '../../components/Filters/FilterComponent'
-import {Card} from '@/components/Re_usable/Card/Card'
+import Image, { StaticImageData } from "next/image";
+import Mondsdadt from '../../public/image/mondsdatd.webp.png'
+import Liyue from '../../public/image/liyue.webp'
+import Inazuma from '../../public/image/inazuma.webp'
+import Sumeru from '../../public/image/sumeru1.webp'
 
 type Props = {};
+type CardProps = {
+  image: string | StaticImageData;
+}
+
+const imgArray = [
+  {
+    id: 1,
+    image: Mondsdadt,
+    name: "mondsdadt"
+  },{
+    id: 2, 
+    image: Liyue,
+    name : "liyue"
+  },{
+    id: 3,
+    image: Inazuma,
+    name: "inazuma"
+  },{
+    id: 4,
+    image: Sumeru,
+    name: "sumeru"
+  }
+]
 
 export default function page({}: Props) {
   const getData = (data: Object[]) =>{
     console.log(data);
   }
-  const arr = [
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },{
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-    {
-      avatar : "this is avatar",
-      cha_name: "this is chaname",
-      vision: "this is vision"
-    },
-
-  ]
   return (
     <div className={styles.characterContainer}>
-      
+      <div className={styles.nationsContainer}>
+        {imgArray.map((item)=>(
+          <NationCard image={item.image} key={item.id} />
+        ))}
+      </div>
     </div>
   );
+}
+
+const NationCard = (props: CardProps) =>{
+  return (
+    <div className={styles.cardContainer}>
+      <Image src={props.image} alt="card Image" className={styles.cardImage}></Image>
+    </div>
+  )
 }
